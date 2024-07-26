3fb60d7071629082a588fcc5bbe26db704a0bf76
weixing-111 <164284143+weixing-111@users.noreply.github.com>
2024-07-26 18:58:51 +0900

    mingzi

--- frontend2/src/layouts/modules/global-header/components/user-avatar.vue
+++ frontend2/src/layouts/modules/global-header/components/user-avatar.vue
@@ -31,9 +31,7 @@ function logout() {
   <ADropdown v-else placement="bottomRight" trigger="click">
     <ButtonIcon>
       <SvgIcon icon="ph:user-circle" class="text-icon-large" />
-      <span class="text-16px font-medium">{{
-        authStore.userInfo.userName
-      }}</span>
+      <span class="text-16px font-medium">小松高橋</span>
     </ButtonIcon>
     <template #overlay>
       <AMenu>
