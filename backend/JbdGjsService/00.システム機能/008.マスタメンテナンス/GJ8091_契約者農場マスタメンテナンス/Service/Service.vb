9b5a23feb41afb487d0c42ba32786469cfda7451
songfeng369 <songfeng@aiplusgroup.com>
2024-08-16 16:14:37 +0900

    refresh

--- backend/JbdGjsService/00.システム機能/008.マスタメンテナンス/GJ8091_契約者農場マスタメンテナンス/Service/Service.vb
+++ backend/JbdGjsService/00.システム機能/008.マスタメンテナンス/GJ8091_契約者農場マスタメンテナンス/Service/Service.vb
@@ -182,9 +182,10 @@ Namespace JBD.GJS.Service.GJ8091
                         Case Enum編集区分.変更       '変更入力
                             If dt.Rows.Count ＝ 0 Then
                                 Return New DaResponseBase("既に削除されています。")
-                            End If
-                            If CDate(dt.Rows(0)("UP_DATE")) > req.KEIYAKUSYA_NOJO.UP_DATE
-                                Return New DaResponseBase("データを更新できません。他のユーザーによって変更された可能性があります。")
+                            Else
+                                If CDate(dt.Rows(0)("UP_DATE")) > req.KEIYAKUSYA_NOJO.UP_DATE
+                                    Return New DaResponseBase("データを更新できません。他のユーザーによって変更された可能性があります。")
+                                End If
                             End If
                         Case Enum編集区分.新規       '新規入力
                             If dt.Rows.Count > 0 Then
