b4a1b6dc9d64b8dab62e7b15c8b5a1c79fc7dd85
gaozhihuiaiplus <gaozhihui@aiplusgroup.com>
2024-08-23 13:54:43 +0900

    8100作成

--- frontend/src/views/GJ80/GJ8100/modules/EditPage.vue
+++ frontend/src/views/GJ80/GJ8100/modules/EditPage.vue
@@ -1,5 +1,69 @@
 <template>
-  <h1>互助基金契約者マスタメンテナンス（基本情報入力）</h1>
+  <a-card
+    :bordered="false"
+    class="mb-2 h-full"
+    :body-style="{
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+    }"
+  >
+    <h1>互助基金契約者マスタメンテナンス（基本情報入力）</h1>
+    <div class="self_adaption_table form max-w-160 flex-1">
+      <a-row>
+        <a-col span="24">
+          <th class="required">適用開始日</th>
+          <td>
+            <DateJp v-model:value="formData.TAX_DATE_FROM" />
+          </td>
+        </a-col>
+        <a-col span="24">
+          <th class="required">適用終了日</th>
+          <td>
+            <a-form-item>
+              <DateJp v-model:value="formData.TAX_DATE_TO" />
+            </a-form-item>
+          </td>
+        </a-col>
+        <a-col span="24">
+          <th class="required">消費税率（%）</th>
+          <td>
+            <a-form-item>
+              <a-input v-model:value="formData.TAX_RITU" :maxlength="30" />
+            </a-form-item>
+          </td> </a-col
+      ></a-row>
+    </div>
+    <div class="mb-2 flex justify-between">
+      <a-space :size="20">
+        <a-button class="warning-btn" @click="save">保存</a-button>
+      </a-space>
+      <a-button type="primary" @click="goList">戻る</a-button>
+    </div>
+  </a-card>
 </template>
-<script setup lang="ts"></script>
-<style lang="scss" scoped></style>
+<script setup lang="ts">
+import { Judgement } from '@/utils/judge-edited'
+import { reactive } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+const editJudge = new Judgement()
+const formData = reactive({
+  TAX_DATE_FROM: undefined,
+  TAX_DATE_TO: undefined,
+  TAX_RITU: undefined,
+})
+
+const goList = () => {
+  editJudge.judgeIsEdited(() => {
+    router.push({ name: route.name })
+  })
+}
+</script>
+<style lang="scss" scoped>
+th {
+  width: 160px;
+}
+</style>
--- frontend/src/views/GJ80/GJ8100/modules/ListPage.vue
+++ frontend/src/views/GJ80/GJ8100/modules/ListPage.vue
@@ -1,5 +1,78 @@
 <template>
-  <h1>消費税率マスタメンテナンス</h1>
+  <a-card
+    :bordered="false"
+    class="mb-2 h-full"
+    :body-style="{
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+    }"
+  >
+    <vxe-table
+      class="h-full flex-1"
+      ref="xTableRef"
+      :column-config="{ resizable: true }"
+      :row-config="{ isCurrent: true, isHover: true }"
+      :data="tableData"
+      :empty-render="{ name: 'NotData' }"
+    >
+      <vxe-column
+        field="TAX_DATE_FROM"
+        title="適用開始日"
+        min-width="80"
+        :resizable="true"
+      >
+      </vxe-column>
+      <vxe-column
+        field="TAX_DATE_TO"
+        title="適用終了日"
+        min-width="160"
+        :resizable="true"
+      >
+      </vxe-column>
+      <vxe-column
+        field="TAX_RITU"
+        title="消費税率（%）"
+        min-width="100"
+        :resizable="true"
+      ></vxe-column>
+    </vxe-table>
+    <div class="my-2 flex justify-between">
+      <a-space :size="20">
+        <a-button type="primary" @click="add">新規登録</a-button>
+        <a-button type="primary" @click="edit">変更（表示）</a-button>
+        <a-button type="primary" danger @click="delete">削除</a-button>
+      </a-space>
+      <close-page />
+    </div>
+  </a-card>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { PageSatatus } from '@/enum'
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { VxeTableInstance } from 'vxe-pc-ui'
+
+const route = useRoute()
+const router = useRouter()
+
+const xTableRef = ref<VxeTableInstance>()
+const tableData = ref([
+  { TAX_DATE_FROM: '1', TAX_DATE_TO: '1', TAX_RITU: '1' },
+  { TAX_DATE_FROM: '1', TAX_DATE_TO: '1', TAX_RITU: '2' },
+  { TAX_DATE_FROM: '1', TAX_DATE_TO: '1', TAX_RITU: '3' },
+])
+
+const add = () => {
+  router.push({
+    name: route.name,
+    query: {
+      status: PageSatatus.New,
+    },
+  })
+}
+const edit = () => {
+  console.log('edit')
+}
+</script>
 <style lang="scss" scoped></style>
