6b069ba1b11f3cc8d00aaf1023de7e18261dd7c7
weixing-111 <164284143+weixing-111@users.noreply.github.com>
2024-08-27 17:44:57 +0900

    fix

--- frontend/src/views/preview/index.vue
+++ frontend/src/views/preview/index.vue
@@ -8,6 +8,7 @@ import '@grapecity/ar-viewer-ja/dist/jsViewer.min.css'
 import '@grapecity/ar-viewer-ja'
 import { createViewer } from '@grapecity/ar-viewer-ja'
 import { sessionStg } from '@/utils/storage'
+import dayjs from 'dayjs'
 
 //--------------------------------------------------------------------------
 //データ定義
@@ -18,17 +19,6 @@ const host = window.location.href.includes('localhost')
 const URL = computed(() => {
   return `http://${host}:5109/api/reporting`
 })
-const getCurrentFormattedTime = () => {
-  const now = new Date()
-  const year = now.getFullYear().toString()
-  const month = (now.getMonth() + 1).toString().padStart(2, '0')
-  const day = now.getDate().toString().padStart(2, '0')
-  const hours = now.getHours().toString().padStart(2, '0')
-  const minutes = now.getMinutes().toString().padStart(2, '0')
-  const seconds = now.getSeconds().toString().padStart(2, '0')
-
-  return year + month + day + hours + minutes + seconds
-}
 //--------------------------------------------------------------------------
 //フック関数
 //--------------------------------------------------------------------------
@@ -53,7 +43,7 @@ channel.onmessage = (event) => {
             FileName: {
               value:
                 'GJ1030家庭防疫互助基金契約者一覧表(連絡用)_' +
-                getCurrentFormattedTime(),
+                dayjs(new Date()).format('YYYYMMDDHHmmss'),
               visible: true,
             },
           },
@@ -61,7 +51,7 @@ channel.onmessage = (event) => {
             FileName: {
               value:
                 'GJ1030家庭防疫互助基金契約者一覧表(連絡用)_' +
-                getCurrentFormattedTime(),
+                dayjs(new Date()).format('YYYYMMDDHHmmss'),
               visible: true,
             },
           },
@@ -69,7 +59,7 @@ channel.onmessage = (event) => {
             FileName: {
               value:
                 'GJ1030家庭防疫互助基金契約者一覧表(連絡用)_' +
-                getCurrentFormattedTime(),
+                dayjs(new Date()).format('YYYYMMDDHHmmss'),
               visible: true,
             },
           },
