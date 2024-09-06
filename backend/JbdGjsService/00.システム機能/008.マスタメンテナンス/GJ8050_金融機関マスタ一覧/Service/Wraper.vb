5ab0e6dc45d42b4bdc6d24680c6aad2d30a52e8f
TangAIplus <124032901+TangAIplus@users.noreply.github.com>
2024-09-06 11:42:21 +0900

    金融機関マスタ一覧  8050 API commit

---
+++ backend/JbdGjsService/00.システム機能/008.マスタメンテナンス/GJ8050_金融機関マスタ一覧/Service/Wraper.vb
@@ -0,0 +1,63 @@
+﻿' *******************************************************************
+' 業務名称　: 互助事業システム
+' 機能概要　: 金融機関マスタ一覧
+' 　　　　　　DB項目から画面項目に変換
+' 作成日　　: 2024.07.21
+' 作成者　　: 宋 峰
+' 変更履歴　:
+' *******************************************************************
+
+Namespace JBD.GJS.Service.GJ8050
+
+    ''' <summary>
+    ''' 金融機関マスタ一覧
+    ''' </summary>
+    Public Class Wraper
+        Inherits Common.Wraper
+
+        ''' <summary>
+        ''' 検索処理_金融機関一覧画面処理
+        ''' </summary>
+        Public Shared Function SearchBankResponse(dt As DataTable) As SearchBankResponse
+            Dim res = New SearchBankResponse()
+            'データ結果判定
+            If dt.Rows.Count > 0 Then
+                res.TOTAL_ROW_COUNT = CInt(CStr(dt.Rows(0)("RCNT")))
+                res.TOTAL_PAGE_COUNT = CInt(CStr(dt.Rows(0)("PCNT")))
+                res.KEKKA_LIST = New List(Of SearchBankRowVM)
+                ' dt をループし、List にデータを追加します。
+                For Each row As DataRow In dt.Rows
+                    Dim item As New SearchBankRowVM
+                    item.BANK_CD = CStr(row("BANK_CD"))
+                    item.BANK_KANA = CStr(row("BANK_KANA"))
+                    item.BANK_NAME = CStr(row("BANK_NAME"))
+                    res.KEKKA_LIST.Add(item)
+                Next
+            End If
+            Return res
+        End Function
+
+        ''' <summary>
+        ''' 検索処理_支店一覧画面処理
+        ''' </summary>
+        Public Shared Function SearchSitenResponse(dt As DataTable) As SearchSitenResponse
+            Dim res = New SearchSitenResponse()
+            'データ結果判定
+            If dt.Rows.Count > 0 Then
+                res.TOTAL_ROW_COUNT = CInt(CStr(dt.Rows(0)("RCNT")))
+                res.TOTAL_PAGE_COUNT = CInt(CStr(dt.Rows(0)("PCNT")))
+                res.KEKKA_LIST = New List(Of SearchSitenRowVM)
+                ' dt をループし、List にデータを追加します。
+                For Each row As DataRow In dt.Rows
+                    Dim item As New SearchSitenRowVM
+                    item.BANK_CD = CStr(row("BANK_CD"))
+                    item.SITEN_CD = CStr(row("SITEN_CD"))
+                    item.SITEN_KANA = CStr(row("SITEN_KANA"))
+                    item.SITEN_NAME = CStr(row("SITEN_NAME"))
+                    res.KEKKA_LIST.Add(item)
+                Next
+            End If
+            Return res
+        End Function
+    End Class
+End Namespace
---
+++ backend/JbdGjsService/00.システム機能/008.マスタメンテナンス/GJ8050_金融機関マスタ一覧/WebInterface/Request.vb
@@ -0,0 +1,129 @@
+﻿' *******************************************************************
+' 業務名称　: 互助事業システム
+' 機能概要　: 金融機関マスタ一覧
+'            リクエストインターフェース
+' 作成日　　: 2024.07.21
+' 作成者　　: 宋 峰
+' 変更履歴　:
+' *******************************************************************
+
+Namespace JBD.GJS.Service.GJ8050
+
+    ''' <summary>
+    ''' 検索処理_金融機関一覧画面処理
+    ''' </summary>
+    Public Class SearchBankRequest
+        Inherits CmSearchRequestBase
+
+        ''' <summary>
+        ''' 金融機関コード
+        ''' </summary>
+        Public Property BANK_CD As String = String.Empty
+
+        ''' <summary>
+        ''' 金融機関名（ｶﾅ）
+        ''' </summary>
+        Public Property BANK_KANA As String = String.Empty
+
+        ''' <summary>
+        ''' 金融機関名（漢字）
+        ''' </summary>
+        Public Property BANK_NAME As String = String.Empty
+
+        ''' <summary>
+        ''' 検索方法
+        ''' </summary>
+        Public Property SEARCH_METHOD As EnumAndOr = EnumAndOr.AndCode
+    End Class
+
+    ''' <summary>
+    ''' 検索処理_支店一覧画面処理
+    ''' </summary>
+    Public Class SearchSitenRequest
+        Inherits CmSearchRequestBase
+
+        ''' <summary>
+        ''' 金融機関コード
+        ''' </summary>
+        Public Property BANK_CD As String = String.Empty
+
+        ''' <summary>
+        ''' 支店コード
+        ''' </summary>
+        Public Property SITEN_CD As String = String.Empty
+
+        ''' <summary>
+        ''' 支店名（ｶﾅ）
+        ''' </summary>
+        Public Property SITEN_KANA As String = String.Empty
+
+        ''' <summary>
+        ''' 支店名（漢字）
+        ''' </summary>
+        Public Property SITEN_NAME As String = String.Empty
+
+        ''' <summary>
+        ''' 検索方法
+        ''' </summary>
+        Public Property SEARCH_METHOD As EnumAndOr = EnumAndOr.AndCode
+    End Class
+
+    ''' <summary>
+    ''' プレビュー処理_金融機関プレビュー画面処理
+    ''' </summary>
+    Public Class PreviewBankRequest
+        Inherits CmSearchRequestBase
+
+        ''' <summary>
+        ''' 金融機関コード
+        ''' </summary>
+        Public Property BANK_CD As String = String.Empty
+
+        ''' <summary>
+        ''' 金融機関名（ｶﾅ）
+        ''' </summary>
+        Public Property BANK_KANA As String = String.Empty
+
+        ''' <summary>
+        ''' 金融機関名（漢字）
+        ''' </summary>
+        Public Property BANK_NAME As String = String.Empty
+
+        ''' <summary>
+        ''' 検索方法
+        ''' </summary>
+        Public Property SEARCH_METHOD As EnumAndOr = EnumAndOr.AndCode
+    End Class
+
+    ''' <summary>
+    ''' プレビュー処理_支店プレビュー画面処理
+    ''' </summary>
+    Public Class PreviewSitenRequest
+        Inherits CmSearchRequestBase
+
+        ''' <summary>
+        ''' 金融機関コード
+        ''' </summary>
+        Public Property BANK_CD As String = String.Empty
+
+        ''' <summary>
+        ''' 支店コード
+        ''' </summary>
+        Public Property SITEN_CD As String = String.Empty
+
+        ''' <summary>
+        ''' 支店名（ｶﾅ）
+        ''' </summary>
+        Public Property SITEN_KANA As String = String.Empty
+
+        ''' <summary>
+        ''' 支店名（漢字）
+        ''' </summary>
+        Public Property SITEN_NAME As String = String.Empty
+
+        ''' <summary>
+        ''' 検索方法
+        ''' </summary>
+        Public Property SEARCH_METHOD As EnumAndOr = EnumAndOr.AndCode
+    End Class
+End Namespace
