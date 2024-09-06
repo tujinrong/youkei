c4aa2334fd46d2c8b616515cb46e99bddd570aab
gaozhihuiaiplus <gaozhihui@aiplusgroup.com>
2024-09-06 12:02:56 +0900

    8030 fix

--- frontend/src/views/GJ80/GJ8030/index.vue
+++ frontend/src/views/GJ80/GJ8030/index.vue
@@ -14,13 +14,15 @@
           <a-col span="12">
             <th class="required">協会名称</th>
             <td>
-              <a-input v-model:value="formData.KYOKAI_NAME" :maxlength="30" />
+              <a-form-item v-bind="validateInfos.KYOKAI_NAME">
+                <a-input v-model:value="formData.KYOKAI_NAME" :maxlength="30"
+              /></a-form-item>
             </td>
           </a-col>
           <a-col span="12">
             <th class="required">支援事業名</th>
             <td>
-              <a-form-item>
+              <a-form-item v-bind="validateInfos.JIGYO_NAME">
                 <a-input v-model:value="formData.JIGYO_NAME" :maxlength="30" />
               </a-form-item>
             </td>
@@ -28,7 +30,7 @@
           <a-col :md="24" :lg="24" :xl="12" :xxl="12">
             <th class="required">役職名</th>
             <td>
-              <a-form-item>
+              <a-form-item v-bind="validateInfos.YAKUMEI">
                 <a-input v-model:value="formData.YAKUMEI" :maxlength="30" />
               </a-form-item>
             </td>
@@ -38,7 +40,7 @@
           <a-col span="12">
             <th class="required">会長名</th>
             <td>
-              <a-form-item>
+              <a-form-item v-bind="validateInfos.KAICHO_NAME">
                 <a-input v-model:value="formData.KAICHO_NAME" :maxlength="30" />
               </a-form-item>
             </td>
@@ -46,9 +48,7 @@
           <a-col span="12">
             <th>予備</th>
             <td>
-              <a-form-item>
-                <a-input v-model:value="formData.YOBI1" :maxlength="30" />
-              </a-form-item>
+              <a-input v-model:value="formData.YOBI1" :maxlength="30" />
             </td>
           </a-col>
         </a-row>
@@ -58,27 +58,27 @@
             <a-row class="flex-1">
               <a-col :md="24" :lg="24" :xl="12" :xxl="10">
                 <td>
-                  <a-input v-model:value="formData.POST" :maxlength="8" />
+                  <a-form-item v-bind="validateInfos.POST">
+                    <a-input v-model:value="formData.POST" :maxlength="8"
+                  /></a-form-item>
                 </td>
               </a-col>
               <a-col :md="24" :lg="24" :xl="24" :xxl="18">
                 <td>
-                  <a-form-item>
+                  <a-form-item v-bind="validateInfos.ADDR1">
                     <a-input v-model:value="formData.ADDR1" />
                   </a-form-item>
                 </td>
               </a-col>
               <a-col :md="24" :lg="24" :xl="24" :xxl="18">
                 <td>
-                  <a-form-item>
-                    <a-input v-model:value="formData.ADDR2" />
-                  </a-form-item>
+                  <a-input v-model:value="formData.ADDR2" />
                 </td>
               </a-col>
               <a-col :md="24" :lg="24" :xl="12" :xxl="6">
                 <th class="required">発行番号・漢字</th>
                 <td>
-                  <a-form-item>
+                  <a-form-item v-bind="validateInfos.HAKKO_NO_KANJI">
                     <a-input
                       v-model:value="formData.HAKKO_NO_KANJI"
                       :maxlength="4"
@@ -89,18 +89,22 @@
             </a-row>
           </a-col>
           <a-col span="24">
-            <th>連絡先１</th>
+            <th class="required">連絡先１</th>
             <a-row class="flex-1">
               <a-col :md="24" :lg="24" :xl="12" :xxl="8">
                 <th class="required">電話１</th>
                 <td>
-                  <a-input v-model:value="formData.TEL1" :maxlength="13" />
+                  <a-form-item v-bind="validateInfos.TEL1">
+                    <a-input v-model:value="formData.TEL1" :maxlength="13"
+                  /></a-form-item>
                 </td>
               </a-col>
               <a-col :md="24" :lg="24" :xl="12" :xxl="8">
                 <th class="required">ＦＡＸ１</th>
                 <td>
-                  <a-input v-model:value="formData.FAX1" :maxlength="13" />
+                  <a-form-item v-bind="validateInfos.FAX1">
+                    <a-input v-model:value="formData.FAX1" :maxlength="13"
+                  /></a-form-item>
                 </td>
               </a-col>
               <a-col :md="24" :lg="24" :xl="12" :xxl="8">
@@ -137,7 +141,9 @@
           <a-col span="24">
             <th class="required">ホームページURL</th>
             <td>
-              <a-input v-model:value="formData.HP_URL" :maxlength="50" />
+              <a-form-item v-bind="validateInfos.HP_URL">
+                <a-input v-model:value="formData.HP_URL" :maxlength="50"
+              /></a-form-item>
             </td>
           </a-col>
         </a-row>
@@ -146,9 +152,9 @@
             <th class="required">振込口座情報</th>
             <a-row class="flex-1">
               <a-col :md="24" :lg="24" :xl="12" :xxl="12">
-                <th>金融機関</th>
+                <th class="required">金融機関</th>
                 <td>
-                  <a-form-item>
+                  <a-form-item v-bind="validateInfos.FURI_BANK_CD">
                     <ai-select
                       v-model:value="formData.FURI_BANK_CD"
                       :options="option1.BANK_LIST"
@@ -159,9 +165,9 @@
                 </td>
               </a-col>
               <a-col :md="24" :lg="12" :xl="12" :xxl="12">
-                <th>本支店</th>
+                <th class="required">本支店</th>
                 <td>
-                  <a-form-item>
+                  <a-form-item v-bind="validateInfos.FURI_BANK_SITEN_CD">
                     <ai-select
                       v-model:value="formData.FURI_BANK_SITEN_CD"
                       :options="option1.SITEN_LIST"
@@ -171,9 +177,9 @@
                 </td>
               </a-col>
               <a-col :md="24" :lg="12" :xl="12" :xxl="6">
-                <th>口座種別</th>
+                <th class="required">口座種別</th>
                 <td>
-                  <a-form-item>
+                  <a-form-item v-bind="validateInfos.FURI_KOZA_SYUBETU">
                     <ai-select
                       v-model:value="formData.FURI_KOZA_SYUBETU"
                       :options="option1.KOZA_SYUBETU_LIST"
@@ -183,33 +189,39 @@
                 </td>
               </a-col>
               <a-col :md="24" :lg="12" :xl="12" :xxl="6">
-                <th>口座番号</th>
+                <th class="required">口座番号</th>
                 <td>
-                  <a-form-item>
+                  <a-form-item v-bind="validateInfos.FURI_KOZA_NO">
                     <a-input-number
                       v-model:value="formData.FURI_KOZA_NO"
                       string-mode
+                      :min="1"
+                      :max="9999999"
+                      :maxlength="7"
                       style="width: 100%"
                     ></a-input-number>
                   </a-form-item>
                 </td>
               </a-col>
               <a-col :md="24" :lg="12" :xl="12" :xxl="6">
-                <th>種別コード</th>
+                <th class="required">種別コード</th>
                 <td>
-                  <a-form-item>
+                  <a-form-item v-bind="validateInfos.FURI_SYUBETU">
                     <a-input-number
                       v-model:value="formData.FURI_SYUBETU"
                       string-mode
+                      :min="1"
+                      :max="99"
+                      :maxlength="2"
                       style="width: 100%"
                     ></a-input-number>
                   </a-form-item>
                 </td>
               </a-col>
               <a-col :md="24" :lg="12" :xl="24" :xxl="24">
-                <th>口座名義人（カナ）</th>
+                <th class="required">口座名義人（カナ）</th>
                 <td>
-                  <a-form-item>
+                  <a-form-item v-bind="validateInfos.FURI_KOZA_MEIGI_KANA">
                     <a-input
                       v-model:value="formData.FURI_KOZA_MEIGI_KANA"
                       :maxlength="40"
@@ -218,9 +230,9 @@
                 </td>
               </a-col>
               <a-col :md="24" :lg="12" :xl="24" :xxl="24">
-                <th>口座名義人（漢字）</th>
+                <th class="required">口座名義人（漢字）</th>
                 <td>
-                  <a-form-item>
+                  <a-form-item v-bind="validateInfos.FURI_KOZA_MEIGI">
                     <a-input
                       v-model:value="formData.FURI_KOZA_MEIGI"
                       :maxlength="40"
@@ -238,104 +250,99 @@
               <a-col :md="24" :lg="12" :xl="12" :xxl="12">
                 <th>金融機関/th></th>
                 <td>
-                  <a-form-item>
-                    <ai-select
-                      v-model:value="formData.KOFU_BANK_CD"
-                      :options="option2.BANK_LIST"
-                      split-val
-                      @change="onChangeKofuBank"
-                    >
-                    </ai-select>
-                  </a-form-item>
+                  <ai-select
+                    v-model:value="formData.KOFU_BANK_CD"
+                    :options="option2.BANK_LIST"
+                    split-val
+                    @change="onChangeKofuBank"
+                  >
+                  </ai-select>
                 </td>
               </a-col>
               <a-col :md="24" :lg="12" :xl="12" :xxl="12">
                 <th>本支店</th>
                 <td>
-                  <a-form-item>
-                    <ai-select
-                      v-model:value="formData.KOFU_BANK_SITEN_CD"
-                      :options="option2.SITEN_LIST"
-                      split-val
-                    >
-                    </ai-select>
-                  </a-form-item>
+                  <ai-select
+                    v-model:value="formData.KOFU_BANK_SITEN_CD"
+                    :options="option2.SITEN_LIST"
+                    split-val
+                  >
+                  </ai-select>
                 </td>
               </a-col>
               <a-col :md="24" :lg="12" :xl="8" :xxl="6">
                 <th>口座種別</th>
                 <td>
-                  <a-form-item>
-                    <ai-select
-                      v-model:value="formData.KOFU_KOZA_SYUBETU"
-                      :options="option2.KOZA_SYUBETU_LIST"
-                      type="number"
-                    >
-                    </ai-select>
-                  </a-form-item>
+                  <ai-select
+                    v-model:value="formData.KOFU_KOZA_SYUBETU"
+                    :options="option2.KOZA_SYUBETU_LIST"
+                    type="number"
+                  >
+                  </ai-select>
                 </td>
               </a-col>
               <a-col :md="24" :lg="12" :xl="8" :xxl="6">
                 <th>口座番号</th>
                 <td>
-                  <a-form-item>
-                    <a-input-number
-                      v-model:value="formData.KOFU_KOZA_NO"
-                      string-mode
-                      style="width: 100%"
-                    >
-                    </a-input-number>
-                  </a-form-item>
+                  <a-input-number
+                    v-model:value="formData.KOFU_KOZA_NO"
+                    string-mode
+                    :min="1"
+                    :max="9999999"
+                    :maxlength="7"
+                    style="width: 100%"
+                  >
+                  </a-input-number>
                 </td>
               </a-col>
               <a-col :md="24" :lg="12" :xl="8" :xxl="6">
                 <th>種別コード</th>
                 <td>
-                  <a-form-item>
-                    <a-input-number
-                      v-model:value="formData.KOFU_SYUBETU"
-                      string-mode
-                      style="width: 100%"
-                    >
-                    </a-input-number>
-                  </a-form-item>
+                  <a-input-number
+                    v-model:value="formData.KOFU_SYUBETU"
+                    string-mode
+                    :min="1"
+                    :max="99"
+                    :maxlength="2"
+                    style="width: 100%"
+                  >
+                  </a-input-number>
                 </td>
               </a-col>
               <a-col :md="24" :lg="12" :xl="8" :xxl="6">
                 <th>コード区分</th>
                 <td>
-                  <a-form-item>
-                    <a-input-number
-                      v-model:value="formData.KOFU_CD_KBN"
-                      string-mode
-                      style="width: 100%"
-                    ></a-input-number>
-                  </a-form-item>
+                  <a-input-number
+                    v-model:value="formData.KOFU_CD_KBN"
+                    string-mode
+                    :min="1"
+                    :max="9"
+                    :maxlength="1"
+                    style="width: 100%"
+                  ></a-input-number>
                 </td>
               </a-col>
               <a-col :md="24" :lg="24" :xl="20" :xxl="6">
                 <th>依頼人コード</th>
                 <td>
-                  <a-form-item>
-                    <a-input-number
-                      v-model:value="formData.KOFU_KAISYA_CD"
-                      string-mode
-                      style="width: 100%"
-                    >
-                    </a-input-number>
-                  </a-form-item>
+                  <a-input-number
+                    v-model:value="formData.KOFU_KAISYA_CD"
+                    string-mode
+                    :maxlength="10"
+                    style="width: 100%"
+                  >
+                  </a-input-number>
                 </td>
               </a-col>
               <a-col :md="24" :lg="24" :xl="20" :xxl="18">
                 <th>振込依頼人名（ﾌﾘｶﾞﾅ）</th>
                 <td>
-                  <a-form-item>
-                    <a-input
-                      v-model:value="formData.KOFU_KAISYA_NAME"
-                      style="width: 100%"
-                    >
-                    </a-input>
-                  </a-form-item>
+                  <a-input
+                    v-model:value="formData.KOFU_KAISYA_NAME"
+                    style="width: 100%"
+                    :maxlength="40"
+                  >
+                  </a-input>
                 </td>
               </a-col>
             </a-row>
@@ -352,7 +359,13 @@ import { onMounted, reactive, watch } from 'vue'
 import { InitDetail, Save, SearchDetail } from './service'
 import { DetailVM } from './type'
 import { showConfirmModal, showSaveModal } from '@/utils/modal'
-import { CLOSE_CONFIRM, SAVE_CONFIRM, SAVE_OK_INFO } from '@/constants/msg'
+import {
+  CLOSE_CONFIRM,
+  ITEM_REQUIRE_ERROR,
+  ITEM_SELECTREQUIRE_ERROR,
+  SAVE_CONFIRM,
+  SAVE_OK_INFO,
+} from '@/constants/msg'
 
 const formData = reactive<DetailVM>({
   KYOKAI_NAME: '',
@@ -386,6 +399,8 @@ const formData = reactive<DetailVM>({
   KOFU_CD_KBN: 0,
   KOFU_KAISYA_CD: 0,
   KOFU_KAISYA_NAME: '',
+  KYOKAI_KBN: 0,
+  TOUROKU_NO: '',
 })
 
 /**振込口座プルダウンリスト */
@@ -402,7 +417,112 @@ const option2 = reactive({
   KOZA_SYUBETU_LIST: [],
 })
 const editJudge = new Judgement('GJ8030')
-const rules = {}
+const rules = reactive({
+  KYOKAI_NAME: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '協会名称'),
+    },
+  ],
+  JIGYO_NAME: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '支援事業名'),
+    },
+  ],
+  YAKUMEI: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '役職名'),
+    },
+  ],
+  KAICHO_NAME: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '会長名'),
+    },
+  ],
+  POST: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '郵便番号'),
+    },
+  ],
+  ADDR1: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '住所1'),
+    },
+  ],
+  HAKKO_NO_KANJI: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '発行番号・漢字'),
+    },
+  ],
+  TEL1: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '電話１'),
+    },
+  ],
+  FAX1: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', 'ＦＡＸ１'),
+    },
+  ],
+  HP_URL: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', 'ホームページURL'),
+    },
+  ],
+  FURI_BANK_CD: [
+    {
+      required: true,
+      message: ITEM_SELECTREQUIRE_ERROR.Msg.replace('{0}', '金融機関'),
+    },
+  ],
+  FURI_BANK_SITEN_CD: [
+    {
+      required: true,
+      message: ITEM_SELECTREQUIRE_ERROR.Msg.replace('{0}', '本支店'),
+    },
+  ],
+  FURI_KOZA_SYUBETU: [
+    {
+      required: true,
+      message: ITEM_SELECTREQUIRE_ERROR.Msg.replace('{0}', '口座種別'),
+    },
+  ],
+  FURI_KOZA_NO: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '口座番号'),
+    },
+  ],
+  FURI_SYUBETU: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '種別コード'),
+    },
+  ],
+  FURI_KOZA_MEIGI_KANA: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '口座名義人（カナ）'),
+      max: 40,
+    },
+  ],
+  FURI_KOZA_MEIGI: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '口座名義人（漢字）'),
+    },
+  ],
+})
+
 const { validate, clearValidate, validateInfos, resetFields } = Form.useForm(
   formData,
   rules
@@ -421,6 +541,7 @@ watch(
   }
 )
 
+//振込　金融機関
 watch(
   () => formData.FURI_BANK_CD,
   async (newValue) => {
@@ -429,6 +550,7 @@ watch(
   }
 )
 
+//支払　金融機関
 watch(
   () => formData.KOFU_BANK_CD,
   async (newValue) => {
@@ -451,17 +573,15 @@ const onChangeKofuBank = () => {
 /** 登録処理 */
 const save = async () => {
   await validate()
-  try {
-    showSaveModal({
-      content: SAVE_CONFIRM.Msg,
-      onOk: async () => {
-        try {
-          await Save({ KYOKAI: formData })
-          message.success(SAVE_OK_INFO.Msg)
-        } catch (error) {}
-      },
-    })
-  } catch (error) {}
+  showSaveModal({
+    content: SAVE_CONFIRM.Msg,
+    onOk: async () => {
+      try {
+        await Save({ KYOKAI: formData })
+        message.success(SAVE_OK_INFO.Msg)
+      } catch (error) {}
+    },
+  })
 }
 
 /** キャンセル処理 */
