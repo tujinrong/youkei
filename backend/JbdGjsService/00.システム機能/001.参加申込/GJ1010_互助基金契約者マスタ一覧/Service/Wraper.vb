db34be16803ddde0853350f57d73226e622a14a3
TangAIplus <124032901+TangAIplus@users.noreply.github.com>
2024-09-11 10:40:15 +0900

    GJ1010 API commit

---
+++ backend/JbdGjsService/00.システム機能/001.参加申込/GJ1010_互助基金契約者マスタ一覧/Service/Wraper.vb
@@ -0,0 +1,94 @@
+﻿' *******************************************************************
+' 業務名称　: 互助事業システム
+' 機能概要　: 互助基金契約者マスタ一覧
+' 　　　　　　DB項目から画面項目に変換
+' 作成日　　: 2024.07.21
+' 作成者　　: 宋 峰
+' 変更履歴　:
+' *******************************************************************
+
+Imports OracleInternal
+
+Namespace JBD.GJS.Service.GJ1010
+
+    ''' <summary>
+    ''' 互助基金契約者マスタ一覧
+    ''' </summary>
+    Public Class Wraper
+        Inherits Common.Wraper
+
+        ''' <summary>
+        ''' 初期化処理_一覧画面
+        ''' </summary>
+        Public Shared Function GetInitResponse(ByVal wKbn As String, req As InitRequest, dt1 As DataTable, dt2 As DataTable, dt3 As DataTable) As InitResponse
+            Dim res = New InitResponse()
+            Dim sWhere As String = String.Empty
+            res.KEN_LIST = New List(Of CmCodeNameModel)
+            res.KEIYAKU_KBN_LIST = New List(Of CmCodeNameModel)
+            res.KEIYAKU_JYOKYO_LIST = New List(Of CmCodeNameModel)
+            ' dt をループし、List にデータを追加します。
+            For Each row As DataRow In dt1.Rows
+                Dim item As New CmCodeNameModel
+                item.CODE = CInt(CStr(row("MEISYO_CD")))
+                item.NAME = CStr(row("MEISYO"))
+                res.KEN_LIST.Add(item)
+            Next
+            For Each row As DataRow In dt2.Rows
+                Dim item As New CmCodeNameModel
+                item.CODE = CInt(CStr(row("MEISYO_CD")))
+                item.NAME = CStr(row("MEISYO"))
+                res.KEIYAKU_KBN_LIST.Add(item)
+            Next
+            For Each row As DataRow In dt3.Rows
+                Dim item As New CmCodeNameModel
+                item.CODE = CInt(CStr(row("MEISYO_CD")))
+                item.NAME = CStr(row("MEISYO"))
+                res.KEIYAKU_JYOKYO_LIST.Add(item)
+            Next
+            If wKbn = "C" Then
+                sWhere = "KI = " & req.KI
+                Dim jdt = f_JimuItaku_Data_Select(sWhere)
+                'データ結果判定
+                If jdt.Rows.Count > 0 Then
+                    res.ITAKU_LIST = New List(Of CmCodeNameModel)
+                    ' dt をループし、List にデータを追加します。
+                    For Each row As DataRow In jdt.Rows
+                        Dim item As New CmCodeNameModel
+                        item.CODE = CInt(row("ITAKU_CD").ToString())
+                        item.NAME = row("ITAKU_NAME").ToString()
+                        res.ITAKU_LIST.Add(item)
+                    Next
+                End If
+            End If
+            Return res
+        End Function
+
+        ''' <summary>
+        ''' 検索処理_一覧画面処理
+        ''' </summary>
+        Public Shared Function SearchResponse(dt As DataTable) As SearchResponse
+            Dim res = New SearchResponse()
+            'データ結果判定
+            If dt.Rows.Count > 0 Then
+                res.TOTAL_ROW_COUNT = CInt(CStr(dt.Rows(0)("RCNT")))
+                res.TOTAL_PAGE_COUNT = CInt(CStr(dt.Rows(0)("PCNT")))
+                res.KEKKA_LIST = New List(Of SearchRowVM)
+                ' dt をループし、List にデータを追加します。
+                For Each row As DataRow In dt.Rows
+                    Dim item As New SearchRowVM
+                    item.KEIYAKUSYA_CD = CInt(CStr(row("KEIYAKUSYA_CD")))
+                    item.KEIYAKUSYA_NAME = CStr(row("KEIYAKUSYA_NAME"))
+                    item.KEIYAKUSYA_KANA = CStr(row("KEIYAKUSYA_KANA"))
+                    item.KEIYAKU_KBN = CInt(CStr(row("KEIYAKU_KBN")))
+                    item.KEIYAKU_KBN_NAME = CStr(row("KEIYAKU_KBN_NM"))
+                    item.KEIYAKU_JYOKYO = CStr(row("KEIYAKU_JYOKYO"))
+                    item.ADDR_TEL1 = CStr(row("ADDR_TEL1"))
+                    item.KEN_CD_NAME = CStr(row("KEN_NM"))
+                    item.ITAKU_RYAKU = CStr(row("ITAKU_RYAKU"))
+                    res.KEKKA_LIST.Add(item)
+                Next
+            End If
+            Return res
+        End Function
+    End Class
+End Namespace
---
+++ backend/JbdGjsService/00.システム機能/001.参加申込/GJ1010_互助基金契約者マスタ一覧/WebInterface/Request.vb
@@ -0,0 +1,92 @@
+﻿' *******************************************************************
+' 業務名称　: 互助事業システム
+' 機能概要　: 互助基金契約者マスタ一覧
+'            リクエストインターフェース
+' 作成日　　: 2024.07.21
+' 作成者　　: 宋 峰
+' 変更履歴　:
+' *******************************************************************
+
+Namespace JBD.GJS.Service.GJ1010
+
+    ''' <summary>
+    ''' 初期化処理_一覧画面処理
+    ''' </summary>
+    Public Class InitRequest
+        Inherits DaRequestBase
+
+        ''' <summary>
+        ''' 期
+        ''' </summary>
+        Public Property KI As Integer = Nothing
+
+    End Class
+
+    ''' <summary>
+    ''' 検索処理_一覧画面処理
+    ''' </summary>
+    Public Class SearchRequest
+        Inherits CmSearchRequestBase
+
+        ''' <summary>
+        ''' 期
+        ''' </summary>
+        Public Property KI As Integer = Nothing
+
+        ''' <summary>
+        ''' 都道府県
+        ''' </summary>
+        Public Property KEN_CD As CmCodeFmToModel = New CmCodeFmToModel
+
+        ''' <summary>
+        ''' 未継続・未契約者を除く
+        ''' </summary>
+        Public Property NOZOKU_FLG As Boolean = True
+
+        ''' <summary>
+        ''' 契約者番号
+        ''' </summary>
+        Public Property KEIYAKUSYA_CD As Integer? = Nothing
+
+        ''' <summary>
+        ''' 契約区分
+        ''' </summary>
+        Public Property KEIYAKU_KBN As Integer? = Nothing
+
+        ''' <summary>
+        ''' 契約状況
+        ''' </summary>
+        Public Property KEIYAKU_JYOKYO As Integer? = Nothing
+
+        ''' <summary>
+        ''' 契約者名
+        ''' </summary>
+        Public Property KEIYAKUSYA_NAME As String = String.Empty
+
+        ''' <summary>
+        ''' 契約者名（ﾌﾘｶﾞﾅ）
+        ''' </summary>
+        Public Property KEIYAKUSYA_KANA As String = String.Empty
+
+        ''' <summary>
+        ''' 住所
+        ''' </summary>
+        Public Property ADDR As String = String.Empty
+
+        ''' <summary>
+        ''' 電話番号
+        ''' </summary>
+        Public Property ADDR_TEL1 As String = String.Empty
+
+        ''' <summary>
+        ''' 事務委託先
+        ''' </summary>
+        Public Property JIMUITAKU_CD As CmCodeFmToModel = New CmCodeFmToModel
+
+        ''' <summary>
+        ''' 検索方法
+        ''' </summary>
+        Public Property SEARCH_METHOD As EnumAndOr? = EnumAndOr.AndCode
+
+    End Class
+End Namespace
