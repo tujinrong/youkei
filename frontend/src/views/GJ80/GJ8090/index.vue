77ebb51059c5c7831528d39d22cceb12d0f5313f
weixing-111 <164284143+weixing-111@users.noreply.github.com>
2024-09-19 16:56:12 +0900

    8090 popup

--- frontend/src/views/GJ80/GJ8090/index.vue
+++ frontend/src/views/GJ80/GJ8090/index.vue
@@ -1,57 +1,329 @@
+<!------------------------------------------------------------------
+ * 業務名称　: 互助事業システム
+ * 機能概要　: 契約者農場マスタメンテナンス
+ * 　　　　　  画面レイアウト・処理
+ * 作成日　　: 2024.07.24
+ * 作成者　　: wx
+ * 変更履歴　:
+ * ----------------------------------------------------------------->
 <template>
-  <div>
-    <div v-show="status === PageStatus.List" class="h-full">
-      <ListPage />
-    </div>
-    <div
-      v-if="status === PageStatus.New || status === PageStatus.Edit"
-      class="h-full"
-    >
-      <!-- <EditPage :status="status" /> -->
-      <EditPage :status="status" />
-    </div>
+  <div
+    class="h-full min-h-500px flex-col-stretch gap-12px overflow-hidden lt-sm:overflow-auto"
+  >
+    <a-card ref="headRef" :bordered="false">
+      <h1>（GJ8090）契約者農場一覧</h1>
+      <div class="self_adaption_table form mt-1">
+        <a-row>
+          <a-col v-bind="layout">
+            <th class="required">期</th>
+            <td>
+              <a-form-item v-bind="validateInfos.KI">
+                <a-input-number
+                  v-model:value="searchParams.KI"
+                  :min="1"
+                  :max="99"
+                  :maxlength="2"
+                  class="w-full"
+                  @change="getInitData(searchParams.KI, false)"
+                ></a-input-number>
+              </a-form-item>
+            </td>
+          </a-col>
+          <a-col v-bind="layout">
+            <th class="required">契約者</th>
+            <td>
+              <a-form-item v-bind="validateInfos.KEIYAKUSYA_CD">
+                <ai-select
+                  v-model:value="searchParams.KEIYAKUSYA_CD"
+                  :options="KEIYAKUSYA_CD_NAME_LIST"
+                  style="width: 100%"
+                  split-val
+                ></ai-select>
+              </a-form-item>
+            </td>
+          </a-col>
+          <a-col v-bind="layout">
+            <th>農場番号</th>
+            <td>
+              <a-form-item>
+                <a-input-number
+                  v-model:value="searchParams.NOJO_CD"
+                  :min="0"
+                  :max="999"
+                  :maxlength="3"
+                  class="w-full"
+                ></a-input-number>
+              </a-form-item>
+            </td>
+          </a-col>
+          <a-col v-bind="layout">
+            <th>農場名</th>
+            <td>
+              <a-form-item>
+                <a-input
+                  v-model:value="searchParams.NOJO_NAME"
+                  :maxlength="20"
+                ></a-input>
+              </a-form-item>
+            </td>
+          </a-col>
+        </a-row>
+      </div>
+      <div class="my-2 flex">
+        <a-space
+          ><span>検索方法</span>
+          <a-radio-group v-model:value="searchParams.SEARCH_METHOD">
+            <a-radio :value="EnumAndOr.AndCode">すべてを含む(AND)</a-radio>
+            <a-radio :value="EnumAndOr.OrCode">いずれかを含む(OR)</a-radio>
+          </a-radio-group></a-space
+        >
+      </div>
+      <div class="flex">
+        <a-space :size="20">
+          <a-button type="primary" @click="searchAll">検索</a-button>
+          <a-button type="primary" @click="reset">条件クリア</a-button>
+          <a-button type="primary" @click="forwardNew">新規登録</a-button>
+        </a-space>
+        <close-page />
+      </div>
+    </a-card>
+    <a-card :bordered="false" class="sm:flex-1-hidden" ref="cardRef">
+      <a-pagination
+        v-model:current="pageParams.PAGE_NUM"
+        v-model:page-size="pageParams.PAGE_SIZE"
+        :total="totalCount"
+        :page-size-options="['10', '25', '50', '100']"
+        :show-total="(total) => `抽出件数： ${total} 件`"
+        show-less-items
+        show-size-changer
+        class="m-b-1 text-end"
+      />
+      <vxe-table
+        class="mt-2"
+        ref="xTableRef"
+        :column-config="{ resizable: true }"
+        :height="height - 64"
+        :row-config="{ isCurrent: true, isHover: true }"
+        :data="tableData"
+        :sort-config="{ trigger: 'cell', orders: ['desc', 'asc'] }"
+        :empty-render="{ name: 'NotData' }"
+        @cell-dblclick="({ row }) => forwardEdit(row.NOJO_CD)"
+        @sort-change="(e) => changeTableSort(e, toRef(pageParams, 'ORDER_BY'))"
+      >
+        <vxe-column
+          header-align="center"
+          field="NOJO_CD"
+          title="農場コード"
+          width="100"
+          align="right"
+          sortable
+          :params="{ order: 1 }"
+          :resizable="true"
+        >
+          <template #default="{ row }">
+            <a @click="forwardEdit(row.NOJO_CD)">{{ row.NOJO_CD }}</a>
+          </template>
+        </vxe-column>
+        <vxe-column
+          header-align="center"
+          field="NOJO_NAME"
+          title="農場名"
+          min-width="160"
+          sortable
+          :params="{ order: 2 }"
+          :resizable="true"
+        >
+          <template #default="{ row }">
+            <a @click="forwardEdit(row.NOJO_CD)">{{ row.NOJO_NAME }}</a>
+          </template>
+        </vxe-column>
+        <vxe-column
+          header-align="center"
+          field="ADDR"
+          title="農場住所"
+          min-width="500"
+          sortable
+          :params="{ order: 3 }"
+          :resizable="false"
+        ></vxe-column>
+      </vxe-table>
+    </a-card>
   </div>
+  <EditPage
+    v-model:visible="editVisible"
+    :editkbn="editkbn"
+    :params="searchParams"
+    :keys="keyList"
+    :name="name"
+    @getTableList="searchAll"
+  />
 </template>
 
-<script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
-import { PageStatus } from '@/enum'
-import ListPage from './modules/ListPage.vue'
+<script lang="ts" setup>
+import { ref, reactive, toRef, watch, onMounted, computed, nextTick } from 'vue'
+import { EnumAndOr, EnumEditKbn, PageStatus } from '@/enum'
+import useSearch from '@/hooks/useSearch'
+import { ITEM_REQUIRE_ERROR } from '@/constants/msg'
+import { changeTableSort, convertToFullWidth } from '@/utils/util'
+import { useElementSize } from '@vueuse/core'
+import { SearchRowVM, SearchRequest } from '@/views/GJ80/GJ8090/type'
+import { Init, Search } from './service'
 import EditPage from './modules/EditPage.vue'
+import { Form } from 'ant-design-vue'
+import { VxeTableInstance } from 'vxe-table'
 
 //--------------------------------------------------------------------------
 //データ定義
 //--------------------------------------------------------------------------
-const route = useRoute()
+const xTableRef = ref<VxeTableInstance>()
+const createDefaultParams = (): SearchRequest => {
+  return {
+    KI: -1,
+    KEIYAKUSYA_CD: undefined,
+    NOJO_CD: undefined,
+    NOJO_NAME: undefined,
+    SEARCH_METHOD: EnumAndOr.AndCode,
+  } as SearchRequest
+}
+const searchParams = reactive(createDefaultParams())
 
-const status = ref(PageStatus.List)
+const keyList = reactive({
+  KI: undefined,
+  KEIYAKUSYA_CD: undefined,
+  KEIYAKUSYA_NAME: '',
+  NOJO_CD: undefined,
+})
+const KEIYAKUSYA_CD_NAME_LIST = ref<CmCodeNameModel[]>([])
+const tableData = ref<SearchRowVM[]>([])
 
+//表の高さ
+const headRef = ref(null)
+const layout = {
+  md: 24,
+  lg: 12,
+  xl: 8,
+  xxl: 6,
+}
+const cardRef = ref()
+const { height } = useElementSize(cardRef)
+const editVisible = ref(false)
+const editkbn = ref<EnumEditKbn>(EnumEditKbn.Add)
+const name = computed(() => {
+  return (
+    KEIYAKUSYA_CD_NAME_LIST.value.find(
+      (el) => el.CODE === Number(searchParams.KEIYAKUSYA_CD)
+    )?.NAME || ''
+  )
+})
+const rules = reactive({
+  KI: [
+    { required: true, message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '期') },
+  ],
+  KEIYAKUSYA_CD: [
+    {
+      required: true,
+      message: ITEM_REQUIRE_ERROR.Msg.replace('{0}', '契約者'),
+    },
+  ],
+})
+const { validate, clearValidate, validateInfos } = Form.useForm(
+  searchParams,
+  rules
+)
 //--------------------------------------------------------------------------
+//計算定義
+//--------------------------------------------------------------------------
+const isDataSelected = computed(() => {
+  return tableData.value.length > 0 && xTableRef.value?.getCurrentRecord()
+})
+//---------------------------------------------------------------------------
 //フック関数
 //--------------------------------------------------------------------------
 onMounted(() => {
-  if (route.query.status) {
-    status.value = +route.query.status
-  }
+  getInitData(searchParams.KI, true)
 })
 
-//---------------------------------------------------------------------------
-//監視定義
-//--------------------------------------------------------------------------
-watch(
-  () => [route.name, route.query],
-  () => {
-    if (route.name === 'gj80_gj8090') {
-      status.value = route.query.status ? +route.query.status : PageStatus.List
-    }
-  },
-  { deep: true }
-)
+//初期化処理
+const getInitData = (KI: number, initflg: boolean) => {
+  if (!KI && KI !== 0) {
+    return
+  }
+  Init({ KI: KI, EDIT_KBN: EnumEditKbn.Add }).then((res) => {
+    if (initflg) searchParams.KI = res.KI
+    searchParams.KEIYAKUSYA_CD = undefined
+    KEIYAKUSYA_CD_NAME_LIST.value = res.KEIYAKUSYA_CD_NAME_LIST
+    nextTick(() => clearValidate())
+  })
+}
 
 //--------------------------------------------------------------------------
 //メソッド
 //--------------------------------------------------------------------------
+
+async function forwardNew() {
+  await validate()
+  editVisible.value = true
+  editkbn.value = EnumEditKbn.Add
+  console.log(name.value)
+  debugger
+}
+async function forwardEdit(NOJO_CD) {
+  await validate()
+  editVisible.value = true
+  editkbn.value = EnumEditKbn.Edit
+  keyList.NOJO_CD = NOJO_CD
+}
+
+//検索処理
+const { pageParams, totalCount, searchData, clear } = useSearch({
+  service: Search,
+  source: tableData,
+  params: toRef(() => searchParams),
+  validate,
+})
+
+//クリア
+async function reset() {
+  searchParams.NOJO_CD = undefined
+  searchParams.NOJO_NAME = undefined
+  searchParams.SEARCH_METHOD = EnumAndOr.AndCode
+  getInitData(-1, true)
+  xTableRef.value?.clearSort()
+  clear()
+}
+
+const searchAll = async () => {
+  tableData.value = []
+  const res = await searchData()
+  keyList.KI = res.KI
+  keyList.KEIYAKUSYA_CD = res.KEIYAKUSYA_CD
+  keyList.KEIYAKUSYA_NAME =
+    KEIYAKUSYA_CD_NAME_LIST.value.find((el) => el.CODE === res.KEIYAKUSYA_CD)
+      ?.NAME || ''
+  if (xTableRef.value && tableData.value.length > 0) {
+    xTableRef.value.setCurrentRow(tableData.value[0])
+  }
+}
+//--------------------------------------------------------------------------
+//監視定義
+//--------------------------------------------------------------------------
+watch(
+  () => searchParams.NOJO_NAME,
+  (newVal) => {
+    if (newVal) {
+      searchParams.NOJO_NAME = convertToFullWidth(newVal)
+    }
+  }
+)
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+:deep(th) {
+  min-width: 100px;
+}
+
+:deep(.ant-form-item) {
+  margin-bottom: 0;
+  width: 100%;
+}
+</style>
