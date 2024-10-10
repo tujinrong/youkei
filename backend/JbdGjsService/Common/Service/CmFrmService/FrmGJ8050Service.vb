f0d25f0213c54f32ec3f871de0a66387e4402b44
songfeng <songfeng@aiplusgroup.com>
2024-10-10 14:14:41 +0900

    202410101414

--- backend/JbdGjsService/Common/Service/CmFrmService/FrmGJ8050Service.vb
+++ backend/JbdGjsService/Common/Service/CmFrmService/FrmGJ8050Service.vb
@@ -7,8 +7,6 @@
 ' 変更履歴　:
 ' *******************************************************************
 
-Imports OracleInternal.Json
-
 Namespace JBD.GJS.Service.GJ8050
 
     Public Class FrmGJ8050Service
